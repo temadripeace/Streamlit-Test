5ec469524e7b3de6a4a80e7bc52f738e3bb210fc
temadripeace <71979854+temadripeace@users.noreply.github.com>
2025-08-08 11:20:00 +0300

    Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -13,15 +13,7 @@ import streamkit as st
 st.title (" Simple Streamlit App")
 st.header("Welcome")
 st.write ("This is a basic streamlit app")
-
-
-# In[ ]:
-
-
-name = st.text_input("Enter Your Name")
-if name:
-    st.success (f "Hello", (name)! \n "How are you")
-    
+   
 number = st.slider("Pick your age", 0,100)
 st.write(f "your age is" {number})
 
